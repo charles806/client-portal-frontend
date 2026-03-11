f35e36dda359d2c88efe96ec9b8cd5264ee317c5
charles806 <c08445333@gmail.com>
2026-03-11 20:55:26 +0100

    CHah

--- src/app/pages/landing/PricingSection.tsx
+++ src/app/pages/landing/PricingSection.tsx
@@ -162,7 +162,7 @@ const PricingSection: React.FC = () => {
         </div>
 
         {/* Plans grid */}
-        <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start cursor-pointer">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start cursor-pointer">
           {plans.map((plan, i) => (
             <div
               key={plan.name}
